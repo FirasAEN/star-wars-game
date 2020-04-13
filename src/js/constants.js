66cc4fd757c498ee4a9919f3970cc3ca3f3aa338
Firas <firas.abedelnabi@gmail.com>
2020-04-13 21:43:27 +0200

    feat: adds basic template and resources files

---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+const STAR_FIGHTERS = {
+    X_WING: 'src/resources/x_wing.png',
+    ANAKIN: 'src/resources/anakin_starfighter.png',
+    NABOO: 'src/resources/naboo_starfighter.png',
+    OBI_WAN: 'src/resources/obi_wan_starfighter.png',
+};
+
+const VILLAINS = {
+    DARTH_VADER: 'src/resources/darthvader.png',
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-alert("Hello")
+window.addEventListener('load', init, false);
+
+function init() {
+    var img = new Image;
+    var ctx = document.getElementById('play-area').getContext('2d');
+
+    img.src = STAR_FIGHTERS.NABOO;
+    img.addEventListener('load', function () {
+
+        var interval = setInterval(function() {
+            var x = 0, y = 0;
+
+            return function () {
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                ctx.drawImage(img, x, y);
+
+                x += 1;
+                if (x > ctx.canvas.width) {
+                    x = 0;
+                }
+            };
+        }(), 25);
+    }, false);
+}
