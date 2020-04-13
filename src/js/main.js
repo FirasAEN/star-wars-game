1524c7cab88f097fabc5adf708e99aa403b51513
Firas <firas.abedelnabi@gmail.com>
2020-04-14 00:06:40 +0200

    refactor: extract  callbacks

--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,21 @@ function init() {
     img.src = STAR_FIGHTERS.NABOO;
     img.addEventListener('load', function () {
 
-        var interval = setInterval(function() {
-            var x = 0, y = 0;
-
-            return function () {
-                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-                ctx.drawImage(img, x, y);
-
-                x += 1;
-                if (x > ctx.canvas.width) {
-                    x = 0;
-                }
-            };
-        }(), 25);
+        var interval = setInterval(updateImagePosition(ctx, img), 25);
     }, false);
+}
+
+
+function updateImagePosition(ctx, img) {
+        var x = 0, y = 0;
+
+        return function () {
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            ctx.drawImage(img, x, y);
+
+            x += 1;
+            if (x > ctx.canvas.width) {
+                x = 0;
+            }
+        };
 }
